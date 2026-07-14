0fc363ba71d1fe93c205ec96d33c82fc159cfd81
Boripath <yeenboripath@gmail.com>
2026-07-14 14:13:45 +0700

    Update project

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,17 @@ function App() {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', height: '100vh', overflow: 'hidden' }}>
       <Header />
-      <main style={{ flex: 1, overflowY: 'auto' }}>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/student-dashboard" element={<StudentDashboard />} />
-          <Route path="/teacher-dashboard" element={<TeacherDashboard />} />
-          <Route path="/exam" element={<ExamRoom />} />
-          <Route path="/lesson/:indicator_code" element={<LessonRoom />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
+      <main style={{ flex: 1, display: 'flex', flexDirection: 'column', overflow: 'hidden' }}>
+        <div style={{ flex: 1, overflow: 'hidden' }}>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/student-dashboard" element={<StudentDashboard />} />
+            <Route path="/teacher-dashboard" element={<TeacherDashboard />} />
+            <Route path="/exam" element={<ExamRoom />} />
+            <Route path="/lesson/:indicator_code" element={<LessonRoom />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </div>
         <Footer />
       </main>
     </div>
